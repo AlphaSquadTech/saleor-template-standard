b637c0ccc9d6d2dea04599a71bed97a82df69d19
Hassan Akbar <hassan.akbar@alphasquad.tech>
2026-02-24 22:52:31 +0500

    chore: remove unused hooks and global store files

--- src/hooks/scrollPageTop.ts
+++
@@ -1,9 +0,0 @@
-export const handleScrollToTop = () => {
-  const scrollPosition = window.scrollY || document.documentElement.scrollTop;
-  if (scrollPosition > 700) {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-  }
-};
