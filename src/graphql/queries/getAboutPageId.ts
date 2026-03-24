ca6f077770a43e4cca88d5420a6ff325ee9909f2
Hassan Akbar <hassan.akbar@alphasquad.tech>
2026-03-24 17:58:54 +0500

    chore: remove unused GraphQL mutations and queries for account and checkout operations

--- src/graphql/queries/getAboutPageId.ts
+++
@@ -1,9 +0,0 @@
-import { gql } from "@apollo/client"
-
-export const GET_ABOUT_PAGE_ID = gql`
-  query AboutPageId {
-    pages(first: 10, filter: { slugs: "about" }) {
-      edges { node { id } }
-    }
-  }
-`
--- src/graphql/types/categories.ts
+++
@@ -1,9 +0,0 @@
-export type Category = {
-  id: string;
-  name: string;
-  description: string;
-  slug: string;
-  backgroundImage: {
-    url: string;
-  } | null;
-};
